982ca3b67105c9865086b5a5d0578ca638834bdc
Dmitry Baryshnikov <bishop.dev@gmail.com>
2013-08-22 15:34:29 +0400

    Add about dialog

--- android/src/com/nextgis/metroaccess/MainActivity.java
+++ android/src/com/nextgis/metroaccess/MainActivity.java
@@ -1,22 +1,81 @@
+/******************************************************************************
+ * Project:  Metro Access
+ * Purpose:  Routing in subway for disabled.
+ * Author:   Baryshnikov Dmitriy (aka Bishop), polimax@mail.ru
+ ******************************************************************************
+*   Copyright (C) 2013 NextGIS
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
 package com.nextgis.metroaccess;
 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockActivity {
+	final static String TAG = "metroaccess";	
+
+//TODO:	public final static int MENU_SETTINGS = 4;
+	public final static int MENU_ABOUT = 5;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+        // initialize the default settings
+//TODO:        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+		
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+		//TODO:        menu.add(Menu.NONE, MENU_SETTINGS, Menu.NONE, R.string.sSettings)
+//      .setIcon(R.drawable.ic_action_settings)
+//      .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);		
+		menu.add(Menu.NONE, MENU_ABOUT, Menu.NONE, R.string.sAbout)
+		.setIcon(R.drawable.ic_action_about)
+		.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);	
+		  
 		return true;
+//		return super.onCreateOptionsMenu(menu);
 	}
+
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+        case android.R.id.home:
+            return false;
+//TODO:        case MENU_SETTINGS:
+            // app icon in action bar clicked; go home
+//            Intent intentSet = new Intent(this, PreferencesActivity.class);
+//            intentSet.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(intentSet);
+//            return true;
+        case MENU_ABOUT:
+            Intent intentAbout = new Intent(this, AboutActivity.class);
+            intentAbout.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intentAbout);
+            return true;	  
+        }
+		return super.onOptionsItemSelected(item);
+	}	
 
 }
