66d633b4705f62c2da141e938de8122712fa12da
karavanjo <jownkiv@gmail.com>
2013-09-16 16:46:04 +0300

    loader module was added

---
+++ web/static/m4a/m4a.loader.js
@@ -0,0 +1,31 @@
+var m4a = {};
+
+m4a.viewmodel = {};
+m4a.view = {};
+
+(function ($, m4a) {
+    m4a.loader = {};
+
+    $.extend(m4a.loader, {
+        init: function() {
+            this.setDomOptions();
+
+            m4a.url.init();
+        },
+
+
+        setDomOptions: function() {
+            var view = m4a.view;
+
+            view.$document = $(document);
+
+            view.$metroStartStation = $('#metroStartStation');
+            view.$metroStartInputName = $('#metroStartInputName');
+            view.$metroStartInputID = $('#metroStartInputID');
+
+            view.$metroEndStation = $('#metroEndStation');
+            view.$metroEndInputName = $('#metroEndInputName');
+            view.$metroEndInputID = $('#metroEndInputID');
+        }
+    })
+}) (jQuery, m4a)
