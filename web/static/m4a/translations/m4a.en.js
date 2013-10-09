4cb60070f7b4a90cdec698c967eb83ff979e7805
karavanjo <jownkiv@gmail.com>
2013-10-09 21:20:33 +0300

    translations for profiles section were added

--- web/static/m4a/translations/m4a.en.js
+++ web/static/m4a/translations/m4a.en.js
@@ -3,7 +3,15 @@
 
     $.extend(m4a.resources, {
         profiles: {
-
+            "man_d": "No limitations",
+            "wch_html": "Wheelchair width, cm",
+            "wch_d": "I'm in a wheelchair",
+            "wch_d2": "I'm in a ",
+            "wch_d3": " cm wide wheelchair",
+            "trl_html": "Trolley width, cm",
+            "trl_d": "I am with a trolley",
+            "trl_d2": "I am with a ",
+            "trl_d3": " cm wide trolley"
         }
     })
 })(jQuery, m4a)
