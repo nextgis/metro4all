0ac470f225c28595141785df1e2128f5ec89e4e5
dvaletin <suntehnik@gmail.com>
2013-10-11 16:56:45 +0400

    fix issue #45 Необходимо проверить фикс, загрузив в Play Market как бета-версию.

--- android/src/com/nextgis/metroaccess/StationIndexedExpandableListAdapter.java
+++ android/src/com/nextgis/metroaccess/StationIndexedExpandableListAdapter.java
@@ -29,7 +29,6 @@ import java.util.List;
 import java.util.Map;
 
 import android.content.Context;
-import android.widget.Filter;
 import android.widget.SectionIndexer;
 
 public class StationIndexedExpandableListAdapter extends StationExpandableListAdapter implements SectionIndexer {
