3d7c8249b450d0ac411f550bc1cf392d93429af6
KoGor <KoGor@users.noreply.github.com>
2014-03-24 04:57:25 -0700

    Update metro.py

--- utils/metro.py
+++ utils/metro.py
@@ -17,7 +17,7 @@ stationsDf = pd.read_csv(stationsReportPath, sep=',', encoding='utf-8')
 transfersDf = pd.read_csv(transfersReportPath, sep=',', encoding='utf-8')
 
 
-values = [nodesDf['totalElements'].sum(), stationsDf['routesIn'].sum(), stationsDf['routesOut'].sum(), transfersDf['transferId'].count(), nodesDf['liftAmount'].sum(), nodesDf['surfaceLevelLiftsAmount'].sum(), nodesDf['hallLevelLiftsAmount'].sum(), nodesDf['trainLevelLiftsAmount'].sum(), nodesDf['lift'].sum(), nodesDf['surfaceLevelLifts'].sum(), nodesDf['hallLevelLifts'].sum(), nodesDf['trainLevelLifts'].sum(), nodesDf['pandusAmount'].sum(), nodesDf['pandusAvailableAmount'].sum(), nodesDf['pandus'].sum(), nodesDf['pandusAvailable'].sum(), nodesDf['stairway'].sum(), nodesDf['coupleStairsAmount'].sum(), stationsDf['wheelchairFriendlyRoutes'].sum(), stationsDf['wheelchairFriendlyRoutesAmount'].sum(), stationsDf['handicappedFriendlyRoutes'].sum(), stationsDf['handicappedFriendlyRoutesAmount'].sum(), stationsDf['luggageFriendlyRoutes'].sum(), stationsDf['luggageFriendlyRoutesAmount'].sum()]
+values = [nodesDf['totalElements'].sum(), stationsDf['routesIn'].sum(), stationsDf['routesOut'].sum(), transfersDf['transferId'].count(), nodesDf['liftAmount'].sum(), nodesDf['hallLevelLiftsAmount'].sum(), nodesDf['trainLevelLiftsAmount'].sum(), nodesDf['surfaceLevelLiftsAmount'].sum(), nodesDf['lift'].sum(), nodesDf['hallLevelLifts'].sum(), nodesDf['trainLevelLifts'].sum(), nodesDf['surfaceLevelLifts'].sum(), nodesDf['pandusAmount'].sum(), nodesDf['pandusAvailableAmount'].sum(), nodesDf['pandus'].sum(), nodesDf['pandusAvailable'].sum(), nodesDf['stairway'].sum(), nodesDf['coupleStairsAmount'].sum(), stationsDf['wheelchairFriendlyRoutes'].sum(), stationsDf['wheelchairFriendlyRoutesAmount'].sum(), stationsDf['handicappedFriendlyRoutes'].sum(), stationsDf['handicappedFriendlyRoutesAmount'].sum(), stationsDf['luggageFriendlyRoutes'].sum(), stationsDf['luggageFriendlyRoutesAmount'].sum()]
 index = ['количество элементов инфраструктуры', 'количество маршрутов на вход', 'количество маршрутов на выход', 'количество переходов', 'количество лифтов - всего', 'количество лифтов на уровень перехода', 'количество лифтов на уровень платформы', 'количество лифтов на уровень поверхности', 'количество узлов, оборудованных лифтами любого уровня', 'количество узлов, оборудованных лифтами до уровня перехода', 'количество узлов, оборудованных лифтами до уровня платформы', 'количество узлов, оборудованных лифтами до уровня поверхности', 'количество пандусов - всего', 'количество пандусов, доступных для инвалидов-колясочников', 'количество узлов, оборудованных пандусами', 'количество узлов, оборудованных пандусами, доступными для инвалидов-колясочников', 'количество узлов, оборудованных эскалаторами', 'количество одиночных ступеней', 'количество станций с маршрутами, доступными для инвалидов-колясочников', 'количество маршрутов, доступных для инвалидов-колясочников', 'количество станций с маршрутами, доступными для людей с затруднениями передвижения', 'количество маршрутов, доступных для людей с затруднениями передвижения', 'количество станций с маршрутами, доступными  для людей с детскими колясками и габаритным багажом', 'количество маршрутов, доступных для людей с детскими колясками и габаритным багажом'] 
 result = pd.Series(values, index=index)
 
@@ -27,13 +27,13 @@ stationsDf['routesIn'].sum()
 stationsDf['routesOut'].sum()
 transfersDf['transferId'].count()
 nodesDf['liftAmount'].sum()
-nodesDf['surfaceLevelLiftsAmount'].sum()
 nodesDf['hallLevelLiftsAmount'].sum()
 nodesDf['trainLevelLiftsAmount'].sum()
+nodesDf['surfaceLevelLiftsAmount'].sum()
 nodesDf['lift'].sum()
-nodesDf['surfaceLevelLifts'].sum()
 nodesDf['hallLevelLifts'].sum()
 nodesDf['trainLevelLifts'].sum()
+nodesDf['surfaceLevelLifts'].sum()
 nodesDf['pandusAmount'].sum()
 nodesDf['pandusAvailableAmount'].sum()
 nodesDf['pandus'].sum()
