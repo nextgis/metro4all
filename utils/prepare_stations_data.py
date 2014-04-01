5fca4fbfe4e7ab74fcbafed9c0d9a18afad66c2e
Maxim Dubinin <sim@gis-lab.info>
2014-04-01 13:50:30 +0400

    forgotten script

---
+++ utils/prepare_stations_data.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+
+# example: python prepare_stations_data.py input.csv
+
+import csv
+import os
+import sys
+
+csv_path = sys.argv[1]
+
+fieldmap = (
+    ('id_station', 'id_station'),
+    ('id_line', 'id_line'),
+    ('id_node', 'id_node'),
+    ('name', 'name'),
+    ('name_en', 'name_en'),
+    ('lat', 'lat'),
+    ('lon', 'lon')
+)
+
+input_f = csv.DictReader(open(csv_path, 'rb'), delimiter=',')
+output_f = csv.DictWriter(open(os.path.join(os.path.dirname(csv_path), 'stations.csv'), 'wb'), [target_name for source_name, target_name in fieldmap], delimiter=';')
+
+output_f.writeheader()
+
+for row in input_f:
+    station = dict()
+    for source_name, target_name in fieldmap:
+        if source_name in row.keys():
+            station[target_name] = row[source_name]
+        else:
+            station[target_name] = ''
+    output_f.writerow(station)
