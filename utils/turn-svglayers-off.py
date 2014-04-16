bf3aee20d91558a08888180766773d7e25426498
Maxim Dubinin <sim@gis-lab.info>
2014-04-16 16:26:56 +0400

    one file version

--- utils/turn-svglayers-off.py
+++ utils/turn-svglayers-off.py
@@ -5,44 +5,41 @@
 # Author: Maxim Dubinin (sim@gis-lab.info)
 # About: For each SVG file in a set folder, turn off visibility for specified layers.
 # Created: 07.04.2014
-# Usage example: python turn-svglayers-off.py /usr/local/work/svg
+# Usage example: python turn-svglayers-off.py /usr/local/work/svg/92.svg
 # ---------------------------------------------------------------------------
 
-import glob
 import shutil
 import os,sys
 
 args = sys.argv[ 1: ]
-dir = args[0]
-os.chdir(dir)
+fn = args[0]
 
 turn_off_layers = {'photos','pointers','element-ids','exits'}
 
-for fn in glob.glob("*.svg"):
-    f = open(fn)
-    fn_out = fn.replace(".svg","_out.svg")
-    f_out = open(fn_out, "wb")
-    curlayer = ""
-    change = "no"
+f = open(fn)
+fn_out = fn.replace(".svg","_out.svg")
+f_out = open(fn_out, "wb")
+curlayer = ""
+change = "no"
 
-    for ss in f.readlines():
-        
-        if 'inkscape:label=' in ss:
-            curlayer = ss.replace('inkscape:label=','')
-            curlayer = curlayer.replace('"','')
-            curlayer = curlayer.replace('\n','').strip()
+for ss in f.readlines():
+    
+    if 'inkscape:label=' in ss:
+        curlayer = ss.replace('inkscape:label=','')
+        curlayer = curlayer.replace('"','')
+        curlayer = curlayer.replace('\n','').strip()
 
-        #if layer is empty, it won't have style=display and won't be turned off
-        if 'style="display:' in ss and len(ss) < 30:
-            if curlayer in turn_off_layers:
-                ss = ss.replace("inline","none")
-                print "File: " + fn + ", Layer: " + curlayer+ " changed. String: " + ss.strip().replace("\n","")
-                change = "yes"
+    #if layer is empty, it won't have style=display and won't be turned off
+    if 'style="display:' in ss and len(ss) < 30:
+        if curlayer in turn_off_layers:
+            ss = ss.replace("inline","none")
+            print "File: " + fn + ", Layer: " + curlayer+ " changed. String: " + ss.strip().replace("\n","")
+            change = "yes"
 
-        f_out.write(ss)
+    f_out.write(ss)
 
-    f.close()
-    f_out.close()
+f.close()
+f_out.close()
 
-    if change == "yes":
-        shutil.move(fn_out,fn)
+if change == "yes":
+    shutil.move(fn_out,fn)
