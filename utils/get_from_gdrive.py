6f6ea940795d201cbed64873ba76a711bc3daf04
Maxim Dubinin <sim@gis-lab.info>
2014-04-16 16:13:40 +0400

    fix finding file

--- utils/get_from_gdrive.py
+++ utils/get_from_gdrive.py
@@ -11,7 +11,7 @@
 # Getting started, before running the script:
 # 1. Install Pydrive
 # 2. Go to https://code.google.com/apis/console and create project
-# 3. Go to Credentials, Web applications, Edit, enter http://localhost:8080/ for both ‘Redirect URIs’ and ‘JavaScript origins’.
+# 3. Go to Credentials, Web applications, Edit, enter http://localhost:8080/ for both â€˜Redirect URIsâ€™ and â€˜JavaScript originsâ€™.
 # 4. Download JSON to working folder
 # 5. Add settings.yaml to working folder
 # More: http://pythonhosted.org/PyDrive/quickstart.html
@@ -28,7 +28,7 @@ gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
-file_list = drive.ListFile({'q': 'trashed=true'}).GetList()
+file_list = drive.ListFile({'q': "title contains '" + file_name + "' and trashed = False"}).GetList()
 
 svg = (item for item in file_list if item['title'] == file_name).next()
 file = drive.CreateFile({'id': svg['id']})
