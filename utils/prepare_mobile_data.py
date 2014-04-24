63aa08312fbc0373ac5f6ec35a17ca2367cbc287
Maxim Dubinin <sim@gis-lab.info>
2014-04-25 02:30:05 +0400

    increment version in meta.json

--- utils/prepare_mobile_data.py
+++ utils/prepare_mobile_data.py
@@ -6,38 +6,67 @@
 import csv
 import os
 import sys
+import json
+import codecs
 
-repo_root_path = ".."
-os.chdir(repo_root_path)
 
-city = sys.argv[1]
-stations_csv_in = "data/" + city + "/" + "stations.csv"
+def split_stations(stations_csv_in):
 
-langs = ("ru","en","pl")
+    langs = ("ru","en","pl")
 
-for lang in langs:
-    input_f = csv.DictReader(open(stations_csv_in, 'rb'), delimiter=';')
-    if 'name_' + lang in input_f.fieldnames:
-        stations_csv_out = stations_csv_in.replace(".csv","_" + lang + ".csv")
+    for lang in langs:
+        input_f = csv.DictReader(open(stations_csv_in, 'rb'), delimiter=';')
+        if 'name_' + lang in input_f.fieldnames:
+            stations_csv_out = "temp/" + "stations_" + lang + ".csv"
 
-        stations_fieldmap = (
-            ('id_station', 'id_station'),
-            ('id_line', 'id_line'),
-            ('id_node', 'id_node'),
-            ('name_' + lang, 'name'),
-            ('lat', 'lat'),
-            ('lon', 'lon')
-        )
+            stations_fieldmap = (
+                ('id_station', 'id_station'),
+                ('id_line', 'id_line'),
+                ('id_node', 'id_node'),
+                ('name_' + lang, 'name'),
+                ('lat', 'lat'),
+                ('lon', 'lon')
+            )
 
-        output_f = csv.DictWriter(open(stations_csv_out, 'wb'), [target_name for source_name, target_name in stations_fieldmap], delimiter=';')
+            output_f = csv.DictWriter(open(stations_csv_out, 'wb'), [target_name for source_name, target_name in stations_fieldmap], delimiter=';')
 
-        output_f.writeheader()
+            output_f.writeheader()
 
-        for row in input_f:
-            station = dict()
-            for source_name, target_name in stations_fieldmap:
-                if source_name in row.keys():
-                    station[target_name] = row[source_name]
-                else:
-                    station[target_name] = ''
-            output_f.writerow(station)
+            for row in input_f:
+                station = dict()
+                for source_name, target_name in stations_fieldmap:
+                    if source_name in row.keys():
+                        station[target_name] = row[source_name]
+                    else:
+                        station[target_name] = ''
+                output_f.writerow(station)
+
+def update_meta(city):
+
+    meta = "android/metro4all/data/meta.json"
+    json_content = json.load(codecs.open(meta, 'r', 'utf-8-sig'))
+    packages = [item for item in json_content["packages"]]
+    for package in packages:
+        if package["path"] == city:
+            package["ver"] = packages[0]["ver"] + 1
+
+    j = json.dumps(json_content, ensure_ascii=False).encode('utf8')
+    f = open(meta, 'w')
+    print >> f, j
+    f.close()
+
+if __name__ == '__main__':
+
+    repo_root_path = ".."
+    os.chdir(repo_root_path)
+    if not os.path.exists("temp"):
+        os.makedirs("temp")
+
+    city = sys.argv[1]
+    stations_csv_in = "data/" + city + "/" + "stations.csv"
+
+    split_stations(stations_csv_in)
+    update_meta(city)
+
+
+
