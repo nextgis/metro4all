111bcb013f9c3b9356ad535f9a7fa2633aad6349
Maxim Dubinin <sim@gis-lab.info>
2014-09-23 23:17:15 +0400

    add escalators to interchages

--- utils/prepare_interchanges_data.py
+++ utils/prepare_interchanges_data.py
@@ -18,7 +18,8 @@ fieldmap = (
     ('Лифт отнимает ступенек', 'lift_minus_step'),
     ('Мин. ширина рельс', 'min_rail_width'),
     ('Макс. ширина рельс', 'max_rail_width'),
-    ('Макс. угол', 'max_angle')
+    ('Макс. угол', 'max_angle'),
+    ('Количество эскалаторов на маршруте', 'escalator')
 )
 
 input_f = csv.DictReader(open(csv_path, 'rb'), delimiter=',')
