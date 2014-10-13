3de0088151ecb2a33b576e7131792e2bf4ea04de
Maxim Dubinin <sim@gis-lab.info>
2014-10-13 22:50:20 +0400

    by->be

--- utils/prepare_portals_data.py
+++ utils/prepare_portals_data.py
@@ -13,7 +13,7 @@ fieldmap = (
     ('name_ru', 'name_ru'),
     ('name_en', 'name_en'),
     ('name_pl', 'name_pl'),
-    ('name_by', 'name_by'),
+    ('name_be', 'name_be'),
     ('Код станции', 'id_station'),
     ('Направление', 'direction'),
     ('0_y', 'lat'),
