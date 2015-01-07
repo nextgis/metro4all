93c8c9ccddec9fbfdde3db64fbf8b73473f5511d
Maxim Dubinin <sim@gis-lab.info>
2015-01-07 15:56:39 +0300

    add metro.svg for data packages builds

--- utils/prepare_mobile_data.py
+++ utils/prepare_mobile_data.py
@@ -173,10 +173,10 @@ def createzip(city):
         for filename in files:
             zf.write(os.path.join(dirname, filename))
 
-    for dirname, subdirs, files in os.walk('icons'):
+    #for dirname, subdirs, files in os.walk('icons'):
         #zf.write('icons')
-        for filename in files:
-            zf.write(os.path.join(dirname, filename))
+    #    for filename in files:
+    #        zf.write(os.path.join(dirname, filename))
 
     items = ('stations','lines','portals')
     for lang in langs:
@@ -184,7 +184,10 @@ def createzip(city):
             fn = item + '_' + lang + '.csv'
             if os.path.exists(fn):
                 zf.write(fn)
-
+    
+    os.chdir('icons')
+    zf.write('icons\metro.svg')
+    
     zf.close()
     os.chdir('..')
 
