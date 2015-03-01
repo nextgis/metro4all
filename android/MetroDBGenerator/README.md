f662078ebb861d3693c0d101826c02512d1a2a4d
Stanislav Petryakov <become.glory@yandex.ru>
2015-03-02 00:27:17 +0300

    Clean android dir

--- android/MetroDBGenerator/README.md
+++
@@ -1,3 +0,0 @@
-
-run jar/dbgen.sh <path to .csv files> to generate the DB
-DB will be placed in the same path
--- android/MetroDataCollector/gen/com/nextgis/metro4all/GoodGuy/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.nextgis.metro4all.GoodGuy;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
